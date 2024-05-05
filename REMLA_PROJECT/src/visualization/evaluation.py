c436815ce46a0d3fd0fff554045aed1e28f0dfd1
Smruti Kshirsagar <s.s.kshirsagar@student.tudelft.nl>
2024-05-05 16:41:49 +0200

    Added metrics to dvc

--- REMLA_PROJECT/src/visualization/evaluation.py
+++ REMLA_PROJECT/src/visualization/evaluation.py
@@ -1,7 +1,7 @@
 import pickle
 import seaborn as sns
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score, roc_auc_score, f1_score
+import json
 
 def evaluation():
     """
@@ -21,6 +21,15 @@ def evaluation():
     print(f"Confusion Matrix: {confusion_mat}")
     print(f"Accuracy: {accuracy_score(y_test, y_pred_binary)}")
     sns.heatmap(confusion_mat, annot=True)
+
+    metrics_dict = {
+    "accuracy": round(accuracy_score(y_test, y_pred_binary),5),
+    "roc_auc": round(roc_auc_score(y_test, y_pred_binary),5),
+    "f1": round(f1_score(y_test, y_pred_binary),5)
+    }
+
+    with open("REMLA_PROJECT\\reports\\metrics.json", "w") as json_file:
+        json.dump(metrics_dict, json_file, indent=4)
 
 
 if __name__ == "__main__":
