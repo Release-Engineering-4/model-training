5eaddb1c44dfabeaecab4889615fc67dcade8e71
Razvan Mihai Popescu <R.Popescu-3@student.tudelft.nl>
2024-06-27 18:10:48 +0200

    add coverage badge

---
+++ generate_badge.py
@@ -0,0 +1,26 @@
+import anybadge
+import coverage
+
+cov = coverage.Coverage()
+cov.load()
+
+coverage_percent = cov.report(
+    file=None
+)  
+coverage_percent = float(
+    coverage_percent.strip("%")
+) 
+
+thresholds = {30: "red", 50: "orange", 70: "yellow", 80: "green", 100:"green"}
+
+badge_value = f"{coverage_percent:.1f}%"
+for threshold, color in thresholds.items():
+    if coverage_percent >= threshold:
+        badge_color = color
+    else:
+        break
+
+badge_text = f"Coverage: {badge_value}"
+badge = anybadge.Badge("Coverage", badge_text, badge_color)
+
+badge.write_badge("coverage_badge.svg")
